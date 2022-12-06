75e5ea77a930eeba7f717612665ce9e0ef3c5b82
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-05 22:29:41 -0300

    CRUD completed

--- jogoteca.py
+++ jogoteca.py
@@ -1,12 +1,13 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask     #micro framework
+from flask_sqlalchemy import SQLAlchemy     #BD
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')     #configuracaoes do BD
 
 db = SQLAlchemy(app)
 
-from views import *
+from views import *     #importacao dos viwes
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)     #rerun auto
+
