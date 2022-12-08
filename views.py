4f66c5f0802253a5dd25e768b90b10b290e75277
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-07 22:29:15 -0300

    first attempt to paste image

--- views.py
+++ views.py
@@ -29,6 +29,11 @@ def criar():
     db.session.add(novo_jogo)
     db.session.commit()
 
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}.jpg')
+
+
     return redirect(url_for('index'))
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
@@ -84,3 +89,7 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
