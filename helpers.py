de2f7737fceeb86516c11b934dd5fc209441c925
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-10 19:24:40 -0300

    wtforms implementation

--- helpers.py
+++ helpers.py
@@ -1,7 +1,19 @@
 import os
 from jogoteca import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, SubmitField, PasswordField
 
 
+class FormularioJogo(FlaskForm):
+    nome = StringField('Nome do Jogo', [validators.DataRequired(), validators.length(min=1, max=50)])
+    categoria = StringField('Categoria', [validators.DataRequired(), validators.length(min=1, max=40)])
+    console = StringField('Console', [validators.DataRequired(), validators.length(min=1, max=20)])
+    salvar = SubmitField('salvar')
+
+class FormularioUsuario(FlaskForm):
+    nickname = StringField('Nickname', [validators.DataRequired(), validators.length(min=1, max=20)])
+    senha = PasswordField('senha',[validators.DataRequired(), validators.length(min=1, max=100)] )
+    login = SubmitField('Login')
 def recupera_imagem(id):
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
         if f'capa{id}' in nome_arquivo:
--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,13 @@
 from flask import Flask     #micro framework
 from flask_sqlalchemy import SQLAlchemy     #BD
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')     #configuracaoes do BD
 
 db = SQLAlchemy(app)
+
+csrf = CSRFProtect(app)
 
 from views import *     #importacao dos viwes
 
